29e2e407f7f2ddab2321b624b6511d4ab86181ad
nagaohiroki <testplayers0226@gmail.com>
2020-03-02 14:01:41 +0900

    add game

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+public class Player : MonoBehaviour
+{
+	[SerializeField]
+	float mMoveSpeed = 100.0f;
+	[SerializeField]
+	float mJumpPower = 20.0f;
+	[SerializeField]
+	BoxCollider2D mFoot = null;
+
+	void Carry()
+	{
+	}
+	void Update()
+	{
+		Camera.main.transform.parent.position = transform.position;
+
+		var rigid = GetComponent<Rigidbody2D>();
+		var vec = rigid.velocity;
+		vec.x = Input.GetAxis("Horizontal") * mMoveSpeed * Time.deltaTime;
+		if(Input.GetButtonDown("Jump"))
+		{
+			if(mFoot.IsTouchingLayers(~(1 << LayerMask.NameToLayer("Player"))))
+			{
+				vec.y += mJumpPower * Time.deltaTime;
+			}
+		}
+		rigid.velocity = vec;
+	}
+}
